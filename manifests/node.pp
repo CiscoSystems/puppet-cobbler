c3802cef73ed09fe157df346c4ea93aa2097f81f
Soren Hansen <sorhanse@cisco.com>
2012-04-25 11:36:01 +0000

    Pass root_disk and power settings to cobbler::node

--- manifests/node.pp
+++ manifests/node.pp
@@ -3,7 +3,13 @@ define cobbler::node(
 	$profile,
 	$ip,
 	$domain,
-    $preseed)
+	$preseed,
+	$power_address = "",
+	$power_type = "",
+	$power_user = "",
+	$power_password = "",
+	$power_id = "",
+	$root_disk = '/dev/sda')
 {
 	exec { "cobbler-add-node-${name}":
 		command => "if cobbler system list | grep ${name};
@@ -14,7 +20,7 @@ define cobbler::node(
                         action=add;
                         extra_opts=--netboot-enabled=true;
                     fi;
-                    cobbler system \\\${action} --name='${name}' --mac-address='${mac}' --profile='${profile}' --ip-address=${ip} --dns-name='${name}.${domain}' --hostname='${name}.${domain}' --kickstart='${preseed}' --kopts='netcfg/disable_autoconfig=true netcfg/dhcp_failed=true netcfg/dhcp_options=\"'\"'\"'Configure network manually'\"'\"'\" netcfg/get_nameservers=${cobbler::node_dns} netcfg/get_ipaddress=${ip} netcfg/get_netmask=${cobbler::node_netmask} netcfg/get_gateway=${cobbler::node_gateway} netcfg/confirm_static=true' \\\${extra_opts}",
+                    cobbler system \\\${action} --name='${name}' --mac-address='${mac}' --profile='${profile}' --ip-address=${ip} --dns-name='${name}.${domain}' --hostname='${name}.${domain}' --kickstart='${preseed}' --kopts='netcfg/disable_autoconfig=true netcfg/dhcp_failed=true netcfg/dhcp_options=\"'\"'\"'Configure network manually'\"'\"'\" partman-auto/disk=${root_disk} netcfg/get_nameservers=${cobbler::node_dns} netcfg/get_ipaddress=${ip} netcfg/get_netmask=${cobbler::node_netmask} netcfg/get_gateway=${cobbler::node_gateway} netcfg/confirm_static=true' --power-user=${power_user} --power-address=${power_address} --power-pass=${power_password} --power-id=${power_id} --power-type=${power_type} \\\${extra_opts}",
 		provider => shell,
 		path => "/usr/bin:/bin",
 		require => Package[cobbler],
