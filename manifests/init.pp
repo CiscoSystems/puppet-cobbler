1903a8569fb5172f1c536ab996febd7ed4a3853f
David Nalley <david@gnsa.us>
2012-04-26 16:16:00 -0400

    making the package comply with the puppet style guide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,66 +1,66 @@
 class cobbler(
-	$node_subnet,
-	$node_netmask,
-	$node_gateway,
-	$node_dns,
-	$domain_name,
-	$ip,
-	$proxy = '',
-	$ucs_org = '',
-	$password_crypted = "x")
+  $node_subnet,
+  $node_netmask,
+  $node_gateway,
+  $node_dns,
+  $domain_name,
+  $ip,
+  $proxy = '',
+  $ucs_org = '',
+  $password_crypted = 'x')
 {
-	package { cobbler:
-		ensure => present }
+  package { 'cobbler':
+    ensure => present }
 
-	package { "isc-dhcp-server":	
-		ensure => present }
+  package { 'isc-dhcp-server':
+    ensure => present }
 
-	package { "tftpd-hpa":
-		ensure => present }
+  package { 'tftpd-hpa':
+    ensure => present }
 
-	file { "/etc/cobbler":
-		ensure => directory
-	}
+  file { '/etc/cobbler':
+    ensure => directory
+  }
 
-	file { "/etc/cobbler/settings":
-		content => template('cobbler/settings.erb'),
-		require => File["/etc/cobbler"],
-		notify => Exec["restart-cobbler"],
-	}
+  file { '/etc/cobbler/settings':
+    content => template('cobbler/settings.erb'),
+    require => File['/etc/cobbler'],
+    notify  => Exec['restart-cobbler'],
+  }
 
-	file { "/etc/cobbler/cisco-preseed":
-		content => template('cobbler/preseed.erb'),
-		require => File["/etc/cobbler"],
-	}
+  file { '/etc/cobbler/cisco-preseed':
+    content => template('cobbler/preseed.erb'),
+    require => File['/etc/cobbler'],
+  }
 
-	file { "/etc/cobbler/dhcp.template":
-		content => template('cobbler/dhcp.template.erb'),
-		require => File["/etc/cobbler"],
-		notify => Exec["restart-cobbler"],
-	}
-	
-	file { "/etc/cobbler/power":
-		ensure => directory,
-		require => File["/etc/cobbler"],
-	}
+  file { '/etc/cobbler/dhcp.template':
+    content => template('cobbler/dhcp.template.erb'),
+    require => File['/etc/cobbler'],
+    notify  => Exec['restart-cobbler'],
+  }
 
-	file { "/etc/cobbler/power/power_ucs.template":
-		content => template('cobbler/power_ucs_domain.erb'),
-		require => File["/etc/cobbler/power"],
-	}
+  file { '/etc/cobbler/power':
+    ensure  => directory,
+    require => File['/etc/cobbler'],
+  }
 
-	exec { "restart-cobbler":
-		command => "/usr/sbin/service cobbler restart",
-		refreshonly => true,
-		require => Package[cobbler],
-	}
+  file { '/etc/cobbler/power/power_ucs.template':
+    content => template('cobbler/power_ucs_domain.erb'),
+    require => File['/etc/cobbler/power'],
+  }
 
-	exec { "cobbler-sync":
-		command => "/usr/bin/cobbler sync > /tmp/w.$$ 2>&1",
-        provider => shell,
-		refreshonly => true,
-		before => Exec[restart-cobbler],
-		require => Package[cobbler],
-	}
+  exec { 'restart-cobbler':
+    command     => '/usr/sbin/service cobbler restart',
+    refreshonly => true,
+    require     => Package[cobbler],
+  }
+
+  exec { 'cobbler-sync':
+    command     => '/usr/bin/cobbler sync > /tmp/w.$$ 2>&1',
+    provider    => shell,
+    refreshonly => true,
+    before      => Exec[restart-cobbler],
+    require     => Package[cobbler],
+  }
 
 }
--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -1,9 +1,9 @@
-define cobbler::ubuntu($arch = "x86_64") { 
-    exec { "cobbler-import-$name":
-        command => "if cobbler profile list | grep ${name}-${arch}; then cobbler-ubuntu-import -u ${name}-${arch}; else cobbler-ubuntu-import ${name}-${arch}; fi",
-        provider => shell,
-        path => "/usr/bin:/bin",
-        require => Package[cobbler],
-        before => Exec["restart-cobbler"]
-    }
+define cobbler::ubuntu($arch = 'x86_64') {
+  exec { "cobbler-import-$name":
+    command  => "if cobbler profile list | grep ${name}-${arch}; then cobbler-ubuntu-import -u ${name}-${arch}; else cobbler-ubuntu-import ${name}-${arch}; fi",
+    provider => shell,
+    path     => '/usr/bin:/bin',
+    require  => Package[cobbler],
+    before   => Exec['restart-cobbler']
+  }
 }
