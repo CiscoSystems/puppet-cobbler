c7de33fc17d732ff55e9ca87903d05f8f24b4768
Robert Starmer <rstarmer@gmail.com>
2012-04-28 14:53:00 -0700

    Added documentation to ubuntu.pp
    Described the use to acquire an ubuntu PXE iso for cobbler

--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -1,3 +1,19 @@
+# = Definition: cobbler::ubuntu
+#
+# Add the ubuntu cobbler boot iso(s) for the defined distributions
+#
+# == Parameters:
+#
+# $arch		The Architecture of the iso to be acquired.
+#
+# == Variables:
+#
+# $name		The distribution name (e.g. precise, oneiric, etc.)
+#
+# == Example:
+#
+# cobbler::ubuntu { "precise": }
+#
 define cobbler::ubuntu($arch = "x86_64") { 
     exec { "cobbler-import-$name":
         command => "if cobbler profile list | grep ${name}-${arch}; then cobbler-ubuntu-import -u ${name}-${arch}; else cobbler-ubuntu-import ${name}-${arch}; fi",
