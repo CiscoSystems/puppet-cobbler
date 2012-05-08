a00b8470291738c1905d08727445659fd5694304
Ian Wells <iawells@cisco.com>
2012-05-09 00:10:54 +0200

    Add early as well as late commands; move no-PXE into cobbler's setup; add the pre- and post- triggers for cobbler into the default early and late commands

--- manifests/ubuntu/preseed.pp
+++ manifests/ubuntu/preseed.pp
@@ -1,7 +1,8 @@
 define cobbler::ubuntu::preseed($packages = '',
-                                $early_command = '',
-                                $late_command = '',
+                                $early_command = 'true', # runs before install
+                                $late_command = 'true', # runs after install, before PXE is disabled
                                 $proxy = '',
+				$stop_pxe = true, # true turns off further PXE boots; machine will boot from HD on next reboot
                                 $password_crypted = '') {
 
     if ( ! defined(File['/etc/cobbler/preseeds'])) {
