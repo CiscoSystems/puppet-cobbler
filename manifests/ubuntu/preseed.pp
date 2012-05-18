f005ee6c42bea90d6a38eaef8a607a41905336ae
Robert Starmer <starmer@cisco.com>
2012-05-18 15:39:03 -0700

    updated the preseed.pp
    needed to include the updated parameters to the preseed define.

--- manifests/ubuntu/preseed.pp
+++ manifests/ubuntu/preseed.pp
@@ -1,9 +1,13 @@
-define cobbler::ubuntu::preseed($packages = '',
-                                $early_command = 'true', # runs before install
-                                $late_command = 'true', # runs after install, before PXE is disabled
-                                $proxy = '',
-				$stop_pxe = true, # true turns off further PXE boots; machine will boot from HD on next reboot
-                                $password_crypted = '') {
+define cobbler::ubuntu::preseed(
+  $packages = '',
+  $early_command = 'true', # runs before install
+  $late_command = 'true', # runs after install, before PXE is disabled
+  $proxy = '',
+  $stop_pxe = true, # true turns off further PXE boots; machine will boot from HD on next reboot
+  $password_crypted = '',
+  $expert_disk = false,
+  $diskpart = [],
+  $boot_disk = '/dev/sdc') {
 
     if ( ! defined(File['/etc/cobbler/preseeds'])) {
         file { "/etc/cobbler/preseeds":
