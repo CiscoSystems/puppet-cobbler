d468a716e042fdecd2ba2241f42d283924d5cfc2
Robert Starmer <rstarmer@gmail.com>
2012-05-18 17:24:06 -0700

    trying to deal with extraneous extra lines without adding extra variables

--- manifests/ubuntu/preseed.pp
+++ manifests/ubuntu/preseed.pp
@@ -1,7 +1,7 @@
 define cobbler::ubuntu::preseed(
   $packages = '',
-  $early_command = 'true', # runs before install
-  $late_command = 'true', # runs after install, before PXE is disabled
+  $early_command = false, # runs before install
+  $late_command = false, # runs after install, before PXE is disabled
   $proxy = '',
   $stop_pxe = true, # true turns off further PXE boots; machine will boot from HD on next reboot
   $password_crypted = '',
