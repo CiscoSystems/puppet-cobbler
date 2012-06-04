4ac79fa66debeff98436acc4b1af6ba5f38b85f7
Soren Hansen <sorhanse@cisco.com>
2012-06-04 23:34:09 +0200

    Fix indentation and package selection logic

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 # style systems.
 # 
 # == Local Parameters:
-# - $node_subnet	The subnet for the PXE server, only one subnet is supported
+# - $node_subnet		The subnet for the PXE server, only one subnet is supported
 # - $node_netmask 	The netmask for the subnet
 # - $node_gateway		The default gateway for the subnet
 # - $node_dns				The DNS server IP address (currently cobbler doesn't manage DNS)
@@ -13,9 +13,9 @@
 # - $ip							The IP address that the cobbler server will use as the "next hop"
 # - $dhcp_ip_low		The low range of default DHCP address
 # - $dhcp_ip_high		The high range of default DHCP addresses
-# - $dns_service		Manage DNS? defaults to no. optiosn: manage_dnsmasq, manage_bind
-# - $dhcp_service		Manage DHCP? defaults to no. options: manage_dnsmasq, manage_isc
-# - $ntp_server                 NTP server if the default (ntp.ubuntu.com) isn't reachable
+# - $dns_service		Manage DNS? defaults to no. options: dnsmasq, isc
+# - $dhcp_service		Manage DHCP? defaults to no. options: dnsmasq, isc
+# - $ntp_server			NTP server if the default (ntp.ubuntu.com) isn't reachable
 # - $proxy = ''			If a proxy is required to get to the internet for updates, define it
 # - $password_crypted = "x"  - The default 'localadmin' user password, MD5 encrypted. 
 # 
@@ -27,17 +27,17 @@
 # 
 # Note: the password below is "ubuntu"
 # 
-# node /cobbler\.example\.com/ {
-#  class { cobbler:
-#   node_subnet => "192.168.1.0",
-#   node_netmask => "255.255.255.0",
-#   node_gateway => "192.168.1.1",
-#   node_dns => "192.168.1.1",
-#   ip => '192.168.1.254',
-#   domain_name => "example.com",
-#   proxy => "http://192.168.1.1:3142/",
-#   password_crypted => '$6$UfgWxrIv$k4KfzAEMqMg.fppmSOTd0usI4j6gfjs0962.JXsoJRWa5wMz8yQk4SfInn4.WZ3L/MCt5u.62tHDGB36EhiKF1',
-#  }
+#	node /cobbler\.example\.com/ {
+#		class { cobbler:
+#			node_subnet => "192.168.1.0",
+#			node_netmask => "255.255.255.0",
+#			node_gateway => "192.168.1.1",
+#			node_dns => "192.168.1.1",
+#			ip => '192.168.1.254',
+#			domain_name => "example.com",
+#			proxy => "http://192.168.1.1:3142/",
+#			password_crypted => '$6$UfgWxrIv$k4KfzAEMqMg.fppmSOTd0usI4j6gfjs0962.JXsoJRWa5wMz8yQk4SfInn4.WZ3L/MCt5u.62tHDGB36EhiKF1',
+#	}
 #
 class cobbler(
 	$node_subnet,
@@ -52,29 +52,43 @@ class cobbler(
 	$ucs_org = '',
 	$dns_service = undef,
 	$dhcp_service = undef,
-        $ntp_server = undef,
+	$ntp_server = undef,
 	$password_crypted = "x")
 {
 
 	package { cobbler:
-		ensure => present }
+		ensure => present
+	}
 
-      if $dns_service == $dhcp_service {
-          package { $dns_service:	
-		ensure => present }
-      } else {
-        if $dns_service != undef {
-	   package { $dns_service:
-              ensure => present }
-        }
-        if $dhcp_service != undef {
-            package { $dhcp_service:
-              ensure => present }
-        }
-      }
+	$dns_package = $dns_service ? {
+		'dnsmasq' => 'dnsmasq',
+		'isc' => 'bind9',
+		default => undef,
+	}
+
+	$dhcp_package = $dhcp_service ? {
+		'dnsmasq' => 'dnsmasq',
+		'isc' => 'dhcp3-server',
+		default => undef,
+	}
+
+	if ($dns_package) {
+		package { "$dns_package":
+			ensure => present
+		}
+	}
+  
+	if ($dhcp_package) {
+		if ($dhcp_package != $dns_package) {
+			package { $dhcp_package:
+				ensure => present
+			}
+		}
+	}
 
 	package { "tftpd-hpa":
-		ensure => present }
+		ensure => present
+	}
 
 	file { "/etc/cobbler":
 		ensure => directory
@@ -122,7 +136,7 @@ class cobbler(
 
 	exec { "cobbler-sync":
 		command => "/usr/bin/cobbler sync",
-        provider => shell,
+		provider => shell,
 		refreshonly => true,
 		before => Exec[restart-cobbler],
 		require => Package[cobbler],
