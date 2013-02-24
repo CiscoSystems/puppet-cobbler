f2fb5ccb67b92b9dbc3b276d32854be75c26a4e5
Ian Wells <iawells@cisco.com>
2013-02-24 15:23:25 +0100

    Assorted changes to add new options
    Make no_gateway work (in spite of a broken netcfg - you cannot get through
    netcfg successfully without the gateway being set, but we clear it in
    the late_command)
    
    Added serial console option to cobbler::node
    
    Tidied up cobbler::node kopt code
    
    Made the DHCP range optional; a sensible default is provided for dnsmasq
    so that the statically assigned IPs can be served without a dynamic
    range being added
    
    Removed superfluous no_default_route from the preseed (if it's in
    the preseed it's too late anyway)

--- manifests/node.pp
+++ manifests/node.pp
@@ -15,6 +15,7 @@
 # - $power_password = ""  Power management password
 # - $power_id = ""     Power management port-id/name
 # - $boot_disk = '/dev/sda'  Default Root disk name
+# - $serial = False (true for serial console)
 # - $add_hosts_entry = true, Create a cobbler local hosts entry (also useful for DNS)
 # - $extra_host_aliases = [] Any additional aliases to add to the host entry
 #
@@ -50,10 +51,33 @@ define cobbler::node(
 	$boot_disk = '/dev/sda',
 	$add_hosts_entry = true,
 	$log_host = '',
+	$serial = false,
 	$extra_host_aliases = [])
 {
 
 	$preseed_file="/etc/cobbler/preseeds/$preseed"
+
+	if($cobbler::node_gateway) {
+	    $gateway_opt = "netcfg/get_gateway=${cobbler::node_gateway}"
+	} else {
+	    # There is a bug in Ubuntu's netcfg (as of 2012-09) that
+	    # prevents no-gateway setups working.  This is a workaround
+	    # - we remove the gateway in post-install.
+	    # (no_default_route is conveniently spare)
+	    $gateway_opt = "netcfg/get_gateway=${cobbler::ip} netcfg/no_default_route=true"
+	}
+        if($log_host) {
+            $log_opt = "log_host=${log_host} BOOT_DEBUG=2"
+        } else {
+            $log_opt = ""
+        }
+        if($serial) {
+            $serial_opt = "console=ttyS0,9600"
+        } else {
+            $serial_opt = ""
+        }
+	$net_settings="netcfg/disable_autoconfig=true netcfg/dhcp_failed=true netcfg/dhcp_options=\"'\"'\"'Configure network manually'\"'\"'\" netcfg/get_nameservers=${cobbler::node_dns} netcfg/get_ipaddress=${ip} netcfg/get_netmask=${cobbler::node_netmask} ${gateway_opt} netcfg/confirm_static=true"
+
 	exec { "cobbler-add-node-${name}":
 		command => "if cobbler system list | grep ${name};
                     then
@@ -63,9 +87,7 @@ define cobbler::node(
                         action=add;
                         extra_opts=--netboot-enabled=true;
                     fi;
-		    extra_kargs='';
-		    if [ ! -z \"${log_host}\" ] ; then extra_kargs='log_host=${log_host} BOOT_DEBUG=2' ; fi ;
- 		    cobbler system \\\${action} --name='${name}' --mac-address='${mac}' --profile='${profile}' --ip-address=${ip} --dns-name='${name}.${domain}' --hostname='${name}.${domain}' --kickstart='${preseed_file}' --kopts='netcfg/disable_autoconfig=true netcfg/dhcp_failed=true netcfg/dhcp_options=\"'\"'\"'Configure network manually'\"'\"'\" netcfg/get_nameservers=${cobbler::node_dns} netcfg/get_ipaddress=${ip} netcfg/get_netmask=${cobbler::node_netmask} netcfg/get_gateway=${cobbler::node_gateway} netcfg/confirm_static=true partman-auto/disk=${boot_disk} '\"\\\${extra_kargs}\" --power-user=${power_user} --power-address=${power_address} --power-pass=${power_password} --power-id=${power_id} --power-type=${power_type} \\\${extra_opts}",
+                    cobbler system \\\${action} --name='${name}' --mac-address='${mac}' --profile='${profile}' --ip-address=${ip} --dns-name='${name}.${domain}' --hostname='${name}.${domain}' --kickstart='${preseed_file}' --kopts='priority=critical $net_settings partman-auto/disk=${boot_disk} ${log_opt} ${serial_opt}' --power-user=${power_user} --power-address=${power_address} --power-pass=${power_password} --power-id=${power_id} --power-type=${power_type} \\\${extra_opts}",
 		provider => shell,
 		path => "/usr/bin:/bin",
 		require => Package[cobbler],
